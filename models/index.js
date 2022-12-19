f6d302fba3edc4a7a9b10e49e6579b1f5fc5ae44
Emma Wiggans <ea.wiggans@gmail.com>
2022-12-19 18:34:18 -0500

    category routes mostly completed

--- models/index.js
+++ models/index.js
@@ -4,12 +4,15 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'category_id'
-})
+// // Products belongsTo Category
+// Product.belongsTo(Category, {
+//   foreignKey: 'category_id'
+// })
 
-// Categories have many Products
+// // Categories have many Products
+// Category.hasMany(Product, {
+
+// })
 
 // Products belongToMany Tags (through ProductTag)
 
