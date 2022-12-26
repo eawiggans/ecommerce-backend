3d9f13dbcb2b5acc13df49131885e8db59d0b0c9
Emma Wiggans <ea.wiggans@gmail.com>
2022-12-26 18:01:42 -0500

    updated api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,23 +44,18 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  // try {
-  //   const upCat = await Category.update({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   });
+  try {
+    const cat = await Category.update({
+      where: {
+        id: req.params.id
+      }
+    });
 
-  //   if (!upCat) {
-  //     res.status(404).json({message: 'No category found with that id'});
-  //     return;
-  //   }
-  //   res.status(200).json(upCat);
-  // } catch (err) {
-  //   res.status(500).json(err);
-  // }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
